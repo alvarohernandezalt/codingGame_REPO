a106e07116f8338b3e1550395a8b8a6fe599bca0
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-12-07 13:18:10 +0100

    Create theDescent_02.py

---
+++ theDescent_02.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+# game loop
+while True:
+    mountains = [int(input()) for _ in range(8)]
+    print(max(range(8), key=mountains.__getitem__))
